8f59e3b0431ed46f92d1bb3a0cf83760da61fff3
MUGAIRWA <agalatevinmugairwa@gmail.com>
2025-09-22 13:02:28 +0300

    Updated week 8 Assignment

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ menuOpenButton.addEventListener('click', () => {
 // Close the mobile menu when clicking the close button
 const menuCloseButton = document.querySelector('#menu-close-button');
 menuCloseButton.addEventListener('click', () => menuOpenButton.click());
+
+// initialize swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 25, 
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // responsive break poins
+  breakpoints: {
+    0: {
+        slidePerView: 1
+    },
+    768:{
+        slidePerView: 2
+    },
+    1024: {
+        slidePerView: 3
+    }
+  }
+});
